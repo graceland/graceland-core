cd8219391711c655a08cfcd6fd7ede71ac6a9cb3
Javier Campanini <jmcampanini@gmail.com>
2014-04-20 02:54:50 -0700

    added javadoc for the profiler.

--- graceland-client/src/main/java/io/graceland/client/metrics/MeteredProfiler.java
+++ graceland-client/src/main/java/io/graceland/client/metrics/MeteredProfiler.java
@@ -6,6 +6,13 @@ import com.google.common.base.Preconditions;
 
 import retrofit.Profiler;
 
+/**
+ * A {@link retrofit.Profiler} that records the REST client's performance using the Metrics library.
+ *
+ * @see retrofit.Profiler
+ * @see com.codahale.metrics.MetricRegistry
+ * @see com.codahale.metrics.Timer
+ */
 public class MeteredProfiler implements Profiler<Timer.Context> {
 
     private final Timer timer;
@@ -18,10 +25,26 @@ public class MeteredProfiler implements Profiler<Timer.Context> {
         timer = registry.timer(metricName);
     }
 
+    /**
+     * Return a new instance of the profiler with a specific metric name.
+     *
+     * @param registry   The Metric Registry to record performance.
+     * @param metricName The name of the metric.
+     * @return A new instance of the Profiler.
+     */
     public static MeteredProfiler newInstance(MetricRegistry registry, String metricName) {
         return new MeteredProfiler(registry, metricName);
     }
 
+    /**
+     * Return a new instance of the profiler with the name specific for a class and possibly extra information. It
+     * performs a very similar naming convention to {@link com.codahale.metrics.MetricRegistry#name(Class, String...)}.
+     *
+     * @param registry   The Metric Registry to record performance.
+     * @param klass      A class to base the name of the metric.
+     * @param extraNames Any extra naming information.
+     * @return A new instance of the Profiler.
+     */
     public static MeteredProfiler newInstanceFor(
             MetricRegistry registry,
             Class klass,
